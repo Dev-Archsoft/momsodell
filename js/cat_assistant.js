7678b290798eb3e6c233c3a29d955bc4dc287e04
Dev-Archsoft <33603256+Dev-Archsoft@users.noreply.github.com>
2017-11-13 00:45:40 +0700

    Update cat_assistant.js

--- js/cat_assistant.js
+++ js/cat_assistant.js
@@ -1,24 +1,24 @@
 $(window).load(function() {
-    var sound_cat = $("#cat_assistant_sound").get(0)
-      , is_audio_load = false;
-    if (!isNaN(sound_cat.duration)) {
-        is_audio_load = true;
-        sound_cat.volume = 1.0;
-    }
-    $('.cat_assistant > a').hover(function() {
-        if (is_audio_load) {
-            sound_cat.currentTime = 0;
-            sound_cat.play();
+        var sound_cat = $("#cat_assistant_sound").get(0)
+            , is_audio_load = false;
+        if (!isNaN(sound_cat.duration)) {
+            is_audio_load = true;
+            sound_cat.volume = 1.0;
         }
-        $(this).find(".layer1").stop(true, true).fadeOut(300);
-        $(this).find(".layer2").stop(true, true).fadeIn(100);
+        $('.cat_assistant > a').hover(function() {
+                if (is_audio_load) {
+                    sound_cat.currentTime = 0;
+                    sound_cat.play();
+                }
+                $(this).find(".layer1").stop(true, true).fadeOut(300);
+                $(this).find(".layer2").stop(true, true).fadeIn(100);
+            }
+            , function() {
+                $(this).find(".layer2").stop(true, true).fadeOut(300);
+                $(this).find(".layer1").stop(true, true).fadeIn(100);
+            }
+        ).find(".layer2").css({
+            'display': 'block'
+        }).hide()
     }
-    , function() {
-        $(this).find(".layer2").stop(true, true).fadeOut(300);
-        $(this).find(".layer1").stop(true, true).fadeIn(100);
-    }
-    ).find(".layer2").css({
-        'display': 'block'
-    }).hide()
-}
 );
